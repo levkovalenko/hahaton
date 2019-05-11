6d5734014f6c4339ddb30e5c2e5d2a2e8ea98205
korwin <levozavr@mail.ru>
2019-05-11 17:11:05 +0300

    adding fiels header

--- ml/ml-task/test.py
+++ ml/ml-task/test.py
@@ -1,5 +1,12 @@
 import pandas as pd
+import os
 
-df = pd.read_csv('data/md\\BUday0.csv')
-
-print(df.head(10))
+print(os.listdir(path='data'))
+for file in os.listdir(path='data'):
+    df = pd.read_csv(f'data/{file}')
+    df.loc[-1] = df.columns
+    df.index = df.index + 1
+    df = df.sort_index()
+    df.columns = ['timestamp', 'identifier', 'amount']
+    df.to_csv(f'data/{file}', index = None, header=True)
+    print(df.head(10))
